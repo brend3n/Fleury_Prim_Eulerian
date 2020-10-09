8d34ed031f6432ba17618a3da5e26d15171fb3d4
brend3n <brenden@knights.ucf.edu>
2020-10-09 16:50:48 -0400

    Adding stuff and now moving locations

--- FleuryPrimSolver.java
+++ FleuryPrimSolver.java
@@ -355,6 +355,12 @@ private void init_LM(){
 		S.add(vertex);
 	}
 
+	private void cloneArrayList(ArrayList<Integer> cloner, ArrayList<Integer> clonee){
+		clonee.clear();
+		for(Integer i : cloner){
+			clonee.add(i);
+		}
+	}
 
 	private void init_V_S_Master(){
 		for(int i = 0; i < N ; i++)
@@ -371,11 +377,18 @@ private void init_LM(){
 		}
 	}
 
+	private void reset_V_S_Master(){
+		V_S_Master.clear();
+
+		for(int i = 0; i < N; i++)
+		V_S_Master.add(i);
+	}
 
 	// Initializes the V_S set and updates the current available nodes (removes nodes that have a degree of 0)
 	private void init_V_S(int y){
 		V_S.clear();
-		V_S = (ArrayList)V_S_Master.clone();
+		// V_S = (ArrayList)V_S_Master.clone();
+		cloneArrayList(V_S_Master, V_S);
 	}
 
 	// Removes an element from V_S
@@ -388,7 +401,6 @@ private void init_LM(){
 		}
 
 	}
-
 
 	// Initalizes the Visited array list
 	private void init_Visited(){
@@ -462,6 +474,15 @@ private void init_LM(){
 		return (y == start);
 	}
 
+	private void init_stuff(int x, int y){
+
+	}
+
+	private void doStuff(int x, int y){
+		// update S by adding y to it
+	}
+
+
 	public boolean prims_algo_check(int x, int y, int start){
 
 		int flag = -1;
@@ -479,15 +500,15 @@ private void init_LM(){
 				continue;
 			}
 
-			init_S();
-			init_V_S();
+			init_S(y);
+			init_V_S(y);
 
 			while(!V_S.isEmpty()){
 				for(int i = 0; i < S.size(); i++){
 					flag = 0;
 					for(int j = 0; j < V_S.size(); j++){
 						if(foundLink(i,j)){
-							doStuff();
+							doStuff(x,y);
 							flag = 1;
 							break;
 						}
@@ -579,216 +600,3 @@ private void init_LM(){
 
 
 }
-
-
-/*
-class FleuryPrimSolver{
-
-// Stores the number of nodes in a the graph
-int N;
-
-int graph_size;
-// Creating an adjacency Matrix
-int [][] matrix;
-
-ArrayList<HashSet<Integer>> edges;
-ArrayList<Integer> S = new ArrayList<Integer>();
-ArrayList<Integer> V_S = new ArrayList<Integer>();
-
-//	Constuctor
-public FleuryPrimSolver (String fileName, int numVertices) throws Exception{
-
-
-// Opens the passed in file and allows the filed to be read
-Scanner scan = new Scanner(new File(fileName));
-
-// Storing the number of vertices/nodes in the graph
-this.N = numVertices;
-
-// Declaring a 2-D  integer array to act as the adjacency matrix
-matrix = new int[numVertices][numVertices];
-
-// Not sure what this is for
-int [] edgeCount = new int[N];
-
-
-// Reading in the text file and creating the matrix
-for (int i = 0; i < N; i++){
-
-// Reading in the first charater of each row. This represents the number of the row starting at 0.
-int rowNum = scan.nextInt();
-
-// Ignoring the tab
-scan.skip("\t");
-
-// Capture the entire row as a String to be parsed
-String str = scan.nextLine();
-
-// Setting up the string for parsing.
-// Replacing deleting all commas
-str = str.replace(",", " ");
-
-// Begin to parse string
-Scanner strScan = new Scanner(str);
-
-// Reading each edge in the text file and adding it to the adjacency matrix
-for (int j = 0; j < N; j++){
-if(strScan.hasNextInt())
-matrix[i][j] = strScan.nextInt();
-else
-strScan.skip(" ");
-}
-}
-
-init_VS();
-init_LH();
-}
-
-
-
-
-private void init_VS(){
-for(int i = 0; i < N; i++){
-V_S.add(i);
-}
-}
-
-private void init_LH(){
-edges = new ArrayList<HashSet<Integer>>();
-
-// Initalizing arraylists for each node's set of edges
-for(int i = 0; i < N; i++)
-edges.add(new HashSet<Integer>());
-
-// Storing the neighborhood of each vertex of the graph
-for(int i = 0; i < N; i++)
-for(int j = 0; j < N; j++)
-if(matrix[i][j] == 1){
-edges.get(i).add(j);
-}
-}
-
-
-// Writes the input string (input) into the first line of the output file (filename)
-public static void writeToFile(String filename, String input){
-
-try{
-FileWriter writer = new FileWriter(filename);
-writer.write(input + "\n");
-writer.close();
-
-}catch(IOException f){
-System.out.println("Error in: findContrainedShortestPaths(" + filename +")\n Exception: " + f);
-}
-
-
-}
-
-// Appends the input string (input) to the output file (filename)
-public static void appendToFile(String filename, String input){
-try{
-FileWriter writer = new FileWriter(filename, true);
-writer.write(input);
-writer.write("\n");
-writer.close();
-// System.out.println("Input: " + input +" appended to File: " + filename);
-// System.out.println();
-}catch(IOException f){
-System.out.println("Error in: findContrainedShortestPaths(" + filename +")\n Exception: " + f);
-}
-}
-
-
-// Writes the input string (input) into the first line of the output file (filename)
-public static void writeToFile(String filename, int input){
-
-try{
-FileWriter writer = new FileWriter(filename);
-writer.write(input + "\n");
-writer.close();
-
-}catch(IOException f){
-System.out.println("Error in: FleuryPrimSolver(" + filename +")\n Exception: " + f);
-}
-
-
-}
-
-private static void writeArrayListToFile(String filename, ArrayList<Integer> list){
-for(Integer element : list){
-writeToFile(filename, element);
-}
-}
-
-// Returns the number of edges in the graph
-private int size_of_graph(){
-int degree_sum = 0;
-
-for(int i = 0;i < N; i++)
-for(int j = 0; j < N; j++)
-if(matrix[i][j] == 1)
-degree_sum++;
-
-return degree_sum / 2;
-}
-
-
-private boolean prims(int startPoint, int endPoint){
-
-return true;
-}
-private void Fleuryify(int startPoint){
-
-ArrayList<Integer> circuit = new ArrayList<Integer>();
-
-int graph_size = size_of_graph();
-int currNode = startPoint;
-int newStep = -1;
-
-
-
-// System.out.println(V_S.toString());
-// System.out.println(edges.toString());
-
-while ((graph_size) > 0){
-// System.out.println("graph_size: " + graph_size);
-
-
-for(int i = 0; i < N ; i++){
-System.out.println(edges.get(i).toString());
-}
-
-
-circuit.add(newStep);
-graph_size--;
-// System.out.println(circuit.toString());
-writeArrayListToFile("output.txt", circuit);
-}
-}
-
-
-
-
-public static void main(final String[] args) throws Exception {
-
-if (args.length < 3 ){
-System.out.println("To run this program: ");
-System.out.println("\t javac FleuryPrimSolver [filename] [source] [number of vertices in graph] ");
-// System.out.println("\t java -jar DijkstraSolver.jar [filename of graph] [source vertex] [end vertex] [number of vertices in graph] ");
-
-}else{
-
-
-String fileName = args[0];
-int source = Integer.parseInt(args[1]);
-// int end = Integer.parseInt(args[2]);
-int numberOfVertices = Integer.parseInt(args[2]);
-
-FleuryPrimSolver solver = new FleuryPrimSolver(fileName, numberOfVertices);
-solver.Fleuryify(source);
-}
-
-
-}
-}
-*/
